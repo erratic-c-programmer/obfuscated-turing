5002ea2c3c2c60ba2da13497963f5c069cb6c095
Isaac <isaac.yeo.linux@gmail.com>
2020-08-02 21:05:31 +0800

    DOG

---
+++ turing.c
@@ -0,0 +1,37 @@
+#define                                                                         Machine SEEK_SET
+#define                                                                         machine main
+#define I ,
+#include<stdio.h>
+#define ING EOF
+#define INE read(
+
+
+                char t[256];
+#define T int
+#define S read(u  ,q,1)
+#define NG O_RDONLY
+#define TUR "table"
+#define Q q[0]
+#define TuR u
+#include<unistd.h>
+#define MACH for(;;){
+#include<fcntl.h>
+        /*u is the fd, r is the state, i is the position on the ticker-tape, q is the buffer */
+T u,r,i,n,g;machine
+                                                            (
+                                                           T h,
+                                                          char**
+                                                        m){strcpy(
+                                                     t,m[1]);;;;short
+                                                     q[1];q[0]=0;;TuR
+                                                     =open(TUR I NG)
+                                                     ;;MACH INE u,q,1
+                                                     );;if(Q-'0'-r==0
+                                                     ){S;;S;if(t[i]==
+                                                      Q){S;S;;t[i]=Q
+                                                       ;S;S;;write(
+                                                        1,q,1);;S;
+                                                          S;;Q==
+                                                            'l'
+                                                             ?
+--i:i++;;;;;S;S;r=Q-'0';lseek(u,0,Machine);}else{S;S;S;S;S;S;S;S;S;}}else{S;S;S;S;S;S;S;S;S;S;S;}if(r==' '-'0')break;}}
