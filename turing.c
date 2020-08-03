d41621196a5023ef4457ef2a3dec83d04948dfeb
Isaac <isaac.yeo.linux@gmail.com>
2020-08-03 17:37:22 +0800

    MORE OBFUSCATION

--- turing.c
+++ turing.c
@@ -2,36 +2,75 @@
 #define                                                                         machine main
 #define I ,
 #include<stdio.h>
-#define ING EOF
 #define INE read(
 
 
-                char t[256];
-#define T int
+static char* a,t[256];
+typedef int T;
+#include<stdlib.h>
+#define g T
 #define S read(u  ,q,1)
 #define NG O_RDONLY
+#include<string.h>
 #define TUR "table"
 #define Q q[0]
-#define TuR u
 #include<unistd.h>
 #define MACH for(;;){
 #include<fcntl.h>
-        /*u is the fd, r is the state, i is the position on the ticker-tape, q is the buffer */
-T u,r,i,n,g;machine
-                                                            (
-                                                           T h,
-                                                          char**
-                                                        m){strcpy(
-                                                     t,m[1]);;;;short
-                                                     q[1];q[0]=0;;TuR
-                                                     =open(TUR I NG)
-                                                     ;;MACH INE u,q,1
-                                                     );;if(Q-'0'-r==0
-                                                     ){S;;S;if(t[i]==
-                                                      Q){S;S;;t[i]=Q
-                                                       ;S;S;;write(
-                                                        1,q,1);;S;
-                                                          S;;Q==
-                                                            'l'
-                                                             ?
---i:i++;;;;;S;S;r=Q-'0';lseek(u,0,Machine);}else{S;S;S;S;S;S;S;S;S;}}else{S;S;S;S;S;S;S;S;S;S;S;}if(r==' '-'0')break;}}
+static
+
+
+
+
+
+T u,r,i,n;g machine
+
+(                  T h,char* /**/ *m                      )
+                           {
+                         n=h=0
+                           ;
+                        strcpy
+                       (t,m[1])
+
+
+
+
+
+
+
+
+
+
+
+;char
+q[1]={/* Did yout know? Girraffes like eating apples and orangutans.
+         * What is Lancelot's favourite colour?
+         * You hould know this one!* /
+         
+         /*of course ye do, laddy!*/0
+
+
+                                }
+                                ;
+                                u=
+                               open
+                             /*YES!*/
+                           (TUR I NG);;
+                             MACH INE
+                               /**/
+                                //
+                        u,q,1);;if(Q-'0'-r
+                         ==0){S;;S;if(!(
+                      t[i]-Q))  {S;S;;t[i
+                       ]=Q;S;S;;write(1,q,
+                      1);;S;S;;Q==0154?--i:
+                     i++;;;;;S;free(a);S;r=Q
+                    -'0';lseek(u,u+-u,Machine
+                   );}else{S;S;S;S;S;S;S;S;S;}
+                  }else{S;S;S;S;S;S;S;S;S;S;S;}
+                 if(0x20-'0'==r)break;;;;;;;;;;;
+                                ;;
+                                ;;
+                                ;;
+                                ;;
+                                }}
